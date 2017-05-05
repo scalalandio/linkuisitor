e124657d98c8178412eb2846914e5101bbee00c4
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2017-05-05 20:08:06 +0200

    Build fix

--- scripts/.sbt-helpers.sh
+++ scripts/.sbt-helpers.sh
@@ -1,6 +1,6 @@
 SbtTestTasks="coverage lock test it:test coverageReport coverageAggregate scalastyle"
 
 alias \
-  sbt="${0:a:h}/sbt.launcher" \
+  sbt="${0:a:h}/sbt" \
   sbtTestAll="sbt $SbtTestTasks " \
 ;
